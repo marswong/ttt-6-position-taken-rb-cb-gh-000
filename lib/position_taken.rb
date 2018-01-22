530b86c2bddc5fe1012e3b27162a34e17b23cefc
Mars Wong <marswong618@gmail.com>
2018-01-22 04:52:11 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,9 @@
 # code your #position_taken? method here!
+
+def position_taken?(board, index)
+  if board.include?(nil) || board[index] == " " || board[index] == ""
+    return false
+  else
+    return true
+  end
+end
